5c70f382b72d9930b36bf045af957bd90b9cec98
Luan Faria <luuan.faria@hotmail.com>
2020-03-13 00:15:27 -0300

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,33 @@
-const modalOverley = document.querySelector('.modal-overlay');
+const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
+  card.addEventListener("click", function () {
+
+    modalOverlay.querySelector('img').src =
+    card.querySelector('img').src
+
+    modalOverlay.querySelector('h2').innerHTML =
+    card.querySelector('.title-content').innerHTML
+
+    modalOverlay.querySelector('p').innerHTML =
+    card.querySelector('.author').innerHTML
+
+    modalOverlay.classList.add('active')
+  })
+}
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+  modalOverlay.classList.remove("active")
+})
+
+
+
+
+/* for (let card of cards) {
   card.addEventListener("click", function(){
     const imageID = card.getAttribute("id");
     modalOverley.classList.add('active')
     modalOverley.querySelector('iframe').src = `../assets/images/${imageID}.PNG`;
   })
-}
-
-document.querySelector('.close-modal').addEventListener("click", function(){
-  modalOverley.classList.remove("active")
-  modalOverley.querySelector('iframe').src = ""
-})
+} */
